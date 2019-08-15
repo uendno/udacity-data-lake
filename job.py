87dfa13cbbaade701db8ef2ef03960203484b7af
Thang Tran <uendno@gmail.com>
2019-08-15 08:39:52 +0700

    fix: Remove credential reading statements

--- job.py
+++ job.py
@@ -1,15 +1,14 @@
 import configparser
 import os
-from pathlib import Path
+import shutil
 
 from etl import run
 
-config = configparser.ConfigParser()
-config.read_file(open('/Users/uendno/.aws/credentials'))
+# config = configparser.ConfigParser()
+# config.read_file(open('/Users/uendno/.aws/credentials'))
 
 # os.environ['AWS_REGION'] = 'us-east-1'
 # os.environ['AWS_ACCESS_KEY_ID'] = config.get('default', 'AWS_ACCESS_KEY_ID')
 # os.environ['AWS_SECRET_ACCESS_KEY'] = config.get('default', 'AWS_SECRET_ACCESS_KEY')
-output_path = 'output'
 
-run('s3a://udacity-dend/song_data/*/*/*/*.json', 's3a://udacity-dend/log_data/*.json', output_path)
+run('s3a://udacity-dend/song_data/*/*/*/*.json', 's3a://udacity-dend/log_data/*.json', 's3a://gotit-data-lake')
